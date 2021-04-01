2990d7800b1697a72cf83a2c13a8f2233109a5af
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2021-04-01 12:36:59 -0300

    Menu listo

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ const burger = document.querySelector('.burgerBtn')
 const linea1 = document.querySelector('.linea1')
 const linea2 = document.querySelector('.linea2')
 const navLinks = document.querySelector('.nav-links')
+const linkMenu = document.querySelectorAll('.linkMenu')
 
 
 burger.addEventListener("click", () => {
@@ -9,3 +10,11 @@ burger.addEventListener("click", () => {
     linea1.classList.toggle('linea1-active')
     linea2.classList.toggle('linea2-active')
 })
+
+linkMenu.forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.classList.toggle('active')
+        linea1.classList.toggle('linea1-active')
+        linea2.classList.toggle('linea2-active')
+    })
+});
